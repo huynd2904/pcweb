3c6e11c0ac7117e42e68455548d1d19ccd1a91f0
huynd2904 <huynd@harmony-at.vn>
2024-07-05 11:21:03 +0700

    first commit

---
+++ pointclouds/PClouds/cloud.js
@@ -0,0 +1,75 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 5677589,
+    "boundingBox": {
+        "lx": 6483393.365,
+        "ly": 5589339.315,
+        "lz": 220.92,
+        "ux": 6483673.12,
+        "uy": 5589619.07,
+        "uz": 500.6749999998882
+    },
+    "tightBoundingBox": {
+        "lx": 6483393.3665,
+        "ly": 5589339.31625,
+        "lz": 220.92175,
+        "ux": 6483673.1185,
+        "uy": 5589616.89825,
+        "uz": 278.81325
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "gps-time",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 2.4227492809295656,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
